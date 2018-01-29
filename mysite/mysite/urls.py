ed475727cc8a4ce2207d2f3da11653ee6b700c1b
Marcus Barão <35901556+MarcusBarao@users.noreply.github.com>
2018-01-29 22:17:09 +0100

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,10 +17,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns =  [
-    path('index/', views.index, name='main-view'),
-    path('bio/<username>/', views.bio, name='bio'),
-    path('articles/<slug:title>/', views.article, name='article-detail'),
-    path('articles/<slug:title>/<int:section>/', views.section, name='article-section'),
-    path('weblog/', include('blog.urls')),
-    ...
+    path('admin/', admin.site.urls),
 ]
